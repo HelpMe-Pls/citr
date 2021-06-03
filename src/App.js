83f17f794a46cd1b3cd68df8a5f50fcfeb6781d4
Random Guy <leekhoi5319990@gmail.com>
2021-06-03 11:31:05 +0700

    jsx

--- src/App.js
+++ src/App.js
@@ -1,30 +1,38 @@
-import React from "react";
 import ReactDOM from "react-dom";
 import Pet from "./Pet";
 
+// const App = () => {
+// 	return React.createElement("div", {}, [
+// 		React.createElement(
+// 			"h1",
+// 			{
+// 				id: "cặc", // inside of the curly braces are the attr like id, className, style...
+// 			},
+// 			"Adopt Me"
+// 		), // the last param is the content for the element
+// 		React.createElement(Pet, {
+// 			name: "Luna",
+// 			animal: "Dog",
+// 			breed: "Havanese",
+// 		}),
+// 		React.createElement(Pet, { name: "Pepper", animal: "Bird", breed: "Cock" }),
+// 		React.createElement(Pet, {
+// 			name: "Misen",
+// 			animal: "Cat",
+// 			breed: "Cặc",
+// 		}),
+// 	]);
+// };
+
 const App = () => {
-	return React.createElement("div", {}, [
-		React.createElement(
-			"h1",
-			{
-				id: "cặc", // inside of the curly braces are the attr like id, className, style...
-			},
-			"Adopt Me"
-		), // the last param is the content for the element
-		React.createElement(Pet, {
-			name: "Luna",
-			animal: "Dog",
-			breed: "Havanese",
-		}),
-		React.createElement(Pet, { name: "Pepper", animal: "Bird", breed: "Cock" }),
-		React.createElement(Pet, {
-			name: "Misen",
-			animal: "Cat",
-			breed: "Cặc",
-		}),
-	]);
+	return (
+		<div>
+			<h1 id="cặc">Adopt Me</h1>
+			<Pet name="Luna" animal="Dog" breed="Havanese" />
+			<Pet name="Pepper" animal="Bird" breed="Cock" />
+			<Pet name="Misen" animal="Cat" breed="Cặc" />
+		</div>
+	);
 };
-ReactDOM.render(
-	React.createElement(App, {}, null),
-	document.getElementById("root")
-);
+
+ReactDOM.render(<App />, document.getElementById("root"));
--- src/Pet.js
+++ src/Pet.js
@@ -1,10 +1,20 @@
-import React from "react";
+// const Pet = (props) => {
+// 	return React.createElement("div", {}, [
+// 		React.createElement("h2", {}, props.name),
+// 		React.createElement("h3", {}, props.animal),
+// 		React.createElement("h3", {}, props.breed),
+// 	]);
+// };
+
 const Pet = (props) => {
-	return React.createElement("div", {}, [
-		React.createElement("h2", {}, props.name),
-		React.createElement("h3", {}, props.animal),
-		React.createElement("h3", {}, props.breed),
-	]);
+	// props (or its destructured form) is only declared for intial component, not when you use it (in App)
+	return (
+		<div>
+			<h2>{props.name}</h2>
+			<h3>{props.animal}</h3>
+			<h3>{props.breed}</h3>
+		</div>
+	);
 };
 
 export default Pet;
